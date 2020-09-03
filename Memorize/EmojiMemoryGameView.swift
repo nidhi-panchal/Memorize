440a27979704839c334280818839aa6678444ed2
Nidhi <nidhipanchal1119@gmail.com>
2020-09-02 21:41:40 -0700

    Updated animations on cards but there's a syntax error

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -6,14 +6,23 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards) { card in
-            CardView(card: card).onTapGesture {
-                self.viewModel.choose(card: card)
+        VStack {
+            Grid(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    withAnimation(.linear(duration: 0.75)) {
+                        self.viewModel.choose(card: card)
+                    }
+                }
+            .padding(5)
             }
-        .padding(5)
+                .foregroundColor(Color.orange)
+                .padding()
+            Button(action: {
+                withAnimation(.easeInOut(duration: 2)) {
+                    self.viewModel.resetGame()
+                }
+            }, label: { Text("New Game") })
         }
-            .foregroundColor(Color.orange)
-            .padding()
     }
 }
 
@@ -33,16 +42,36 @@ struct CardView: View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     // every var inside ZStack would require a self. before it, but calling it with self.body in ^^ does it for you so you don't have to type self everywhere
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
+                if card.isConsumingBonusTime {
+                    Pie(startAngle: Angle.degrees(0), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                        .onAppear {
+                            self.startBonusTimeAnimation()
+                    }
+                } else {
+                    Pie(startAngle: Angle.degrees(0), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                }
+                .padding(5).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
     }
     
--- Memorize/Pie.swift
+++ Memorize/Pie.swift
@@ -13,6 +13,17 @@ struct Pie: Shape {
     var endAngle: Angle
     var clockwise: Bool = false
     
+    // need this to animate; usually need to mark as animatable but a shape is assumed to be so
+    var animatableData: AnimatablePair<Double,Double> {
+        get {
+            AnimatablePair(startAngle.radians, endAngle.radians)
+        }
+        set {
+            startAngle = Angle.radians(newValue.first)
+            endAngle = Angle.radians(newValue.second)
+        }
+    }
+    
     func path(in rect: CGRect) -> Path {
         let center = CGPoint(x: rect.midX, y: rect.midY)
         let radius = min(rect.width, rect.height) / 2
